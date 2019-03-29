39cde6c2dbc1c0ab87638eb405f1ab02051003f4
AndrewKralovec <akrala@yahoo.com>
2019-03-28 21:35:41 -0500

    Creating a control for the overlay

---
+++ FluentTerminal.App.ViewModels/OverlayViewModel.cs
@@ -0,0 +1,53 @@
+﻿using FluentTerminal.App.Services;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentTerminal.App.ViewModels
+{
+    public class OverlayViewModel : ViewModelBase
+    {
+        private readonly IDispatcherTimer _resizeOverlayTimer;
+        private bool _showResizeOverlay = true;
+        private string _resizeOverlayContent = "Test";
+
+        public OverlayViewModel(IDispatcherTimer dispatcherTimer)
+        {
+            _resizeOverlayTimer = dispatcherTimer;
+            _resizeOverlayTimer.Interval = new TimeSpan(0, 0, 2);
+            _resizeOverlayTimer.Tick += OnResizeOverlayTimerFinished;
+        }
+
+        public bool ShowResizeOverlay
+        {
+            get => _showResizeOverlay;
+            set
+            {
+                Set(ref _showResizeOverlay, value);
+                if (value)
+                {
+                    if (_resizeOverlayTimer.IsEnabled)
+                    {
+                        _resizeOverlayTimer.Stop();
+                    }
+                    _resizeOverlayTimer.Start();
+                }
+            }
+        }
+
+        public string ResizeOverlayContent
+        {
+            get => _resizeOverlayContent;
+            set => Set(ref _resizeOverlayContent, value);
+        }
+
+        private void OnResizeOverlayTimerFinished(object sender, object e)
+        {
+            _resizeOverlayTimer.Stop();
+            ShowResizeOverlay = false;
+        }
+
+    }
+}
---
+++ FluentTerminal.App/Views/OverlayView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using FluentTerminal.App.ViewModels;
+using FluentTerminal.App.Views;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace FluentTerminal.App.Views
+{
+    /*
+    public sealed partial class OverlayView : UserControl
+    {
+
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+         Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+        public OverlayView()
+        {
+           InitializeComponent();
+            DataContext = this;
+        }
+    }*/
+
+    public sealed partial class OverlayView : UserControl
+    {
+        private readonly IOverlayView _overlayView;
+
+        public OverlayView(OverlayViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+            _overlayView.Initialize(ViewModel);
+        }
+
+        public OverlayViewModel ViewModel { get; }
+
+    }
+}
