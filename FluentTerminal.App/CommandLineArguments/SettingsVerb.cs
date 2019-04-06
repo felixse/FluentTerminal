82819e8269dfb0c95eee7b20f9693805b84dc496
Andrew Montgomery <darkliquid@darkliquid.co.uk>
2019-04-06 14:41:08 +0100

    feat(cli): Add json import/export of app config
    Add the capability to export and import the application config to/from
    a json file. The JSON file is LocalState\config.json in the app package
    folder. The options are available via the new flags to the settings
    command, e.g. `fluent.exe settings --export` and `fluent.exe settings
    --import`
    
    Import attempts to merge imported config with existing config, and
    refuses to import thigns marked as pre-installed. It attempts to
    merge configuration for pre-installed profiles where those fields
    are editable, but otherwise discards invalid configuration.
    
    Change-Id: Iadb468de5f724396db9a2de6bf3c3ba0d4d7ac2f

--- FluentTerminal.App/CommandLineArguments/SettingsVerb.cs
+++ FluentTerminal.App/CommandLineArguments/SettingsVerb.cs
@@ -5,5 +5,10 @@ namespace FluentTerminal.App.CommandLineArguments
     [Verb("settings")]
     public class SettingsVerb
     {
+        [Option("import")]
+        public bool Import { get; set; }
+
+        [Option("export")]
+        public bool Export { get; set; }
     }
 }
