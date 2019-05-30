d21a37d4f09be87cf9ab96257232c89d5003afd4
Max Risuhin <risuhin.max@gmail.com>
2019-05-31 00:54:39 +0300

    Disable context menu for TextBox controls of SSH connection dialog

---
+++ FluentTerminal.App/Utilities/DisabledContextMenuTextBox.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace FluentTerminal.App.Utilities
+{
+    public sealed class DisabledContextMenuTextBox : TextBox
+    {
+        public DisabledContextMenuTextBox() : base()
+        {
+            ContextMenuOpening += OnContextMenuOpening;
+        }
+
+        private void OnContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
