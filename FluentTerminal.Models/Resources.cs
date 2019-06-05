e59b6ba7eb6af7373522c65a24689c720c96a8b9
Aleksandar Pesic <peske.nis@gmail.com>
2019-06-05 15:45:31 +0200

    Minor changes.

---
+++ FluentTerminal.Models/Resources.cs
@@ -0,0 +1,10 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace FluentTerminal.Models
+{
+    public static class Resources
+    {
+        public static string GetString(string resourceId) =>
+            ResourceLoader.GetForCurrentView().GetString(resourceId.Replace('.', '/'));
+    }
+}
--- FluentTerminal.Models/SshConnectionInfoExtensions.cs
+++ FluentTerminal.Models/SshConnectionInfoExtensions.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using FluentTerminal.Models.Enums;
-using Windows.ApplicationModel.Resources;
+﻿using FluentTerminal.Models.Enums;
 
 namespace FluentTerminal.Models
 {
-    public static class Extensions
+    public static class SshConnectionInfoExtensions
     {
-        public static string GetResource(this string resource) =>
-            ResourceLoader.GetForCurrentView().GetString(resource.Replace('.', '/'));
-
         public static SshConnectionInfoValidationResult GetValidationResult(this ISshConnectionInfo sshConnectionInfo,
             bool allowNoUser = false)
         {
@@ -49,24 +42,5 @@ namespace FluentTerminal.Models
             return result;
         }
 
-        public static string GetErrorString(this SshConnectionInfoValidationResult result, string separator = "; ") =>
-            string.Join(separator, result.GetErrors());
-
-        public static IEnumerable<string> GetErrors(this SshConnectionInfoValidationResult result)
-        {
-            if (result == SshConnectionInfoValidationResult.Valid)
-            {
-                yield break;
-            }
-
-            foreach (var value in Enum.GetValues(typeof(SshConnectionInfoValidationResult))
-                .Cast<SshConnectionInfoValidationResult>().Where(r => r != SshConnectionInfoValidationResult.Valid))
-            {
-                if ((value & result) == value)
-                {
-                    yield return $"{nameof(SshConnectionInfoValidationResult)}.{value}".GetResource();
-                }
-            }
-        }
     }
 }
