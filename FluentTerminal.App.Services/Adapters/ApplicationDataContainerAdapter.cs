felixse/FluentTerminal

commit 0065827bf77a7a12213ef2b2fd0d1c0c2d24f5c3

Implementing remote SSH profiles.

FluentTerminal.App.Services/Adapters/ApplicationDataContainerAdapter.cs
@@ -51,7 +51,12 @@ namespace FluentTerminal.App.Services.Adapters
 
         public void WriteValueAsJson<T>(string name, T value)
         {
-            _applicationDataContainer.Values[name] = JsonConvert.SerializeObject(value);
+            //_applicationDataContainer.Values[name] = JsonConvert.SerializeObject(value);
+            _applicationDataContainer.Values[name] = JsonConvert.SerializeObject(value, new JsonSerializerSettings()
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.All,
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+            });
         }
     }
 }

FluentTerminal.App.Services/Implementation/TrayProcessCommunicationService.cs
@@ -73,6 +73,62 @@ namespace FluentTerminal.App.Services.Implementation
             return _userName;
         }
 
+        private string _moshPath;
+        private string _sshPath;
+
+        public async Task<string> GetMoshSshPath(bool isMosh)
+        {
+            if (isMosh)
+            {
+                if (_moshPath != null)
+                {
+                    return _moshPath;
+                }
+            }
+            else if (_sshPath != null)
+            {
+                return _sshPath;
+            }
+
+            GetMoshSshExecutablePathResponse response;
+
+            try
+            {
+                var responseMessage =
+                    await _appServiceConnection.SendMessageAsync(CreateMessage(new GetMoshSshExecutablePathRequest
+                        { IsMosh = isMosh }));
+
+                response = JsonConvert.DeserializeObject<GetMoshSshExecutablePathResponse>(
+                    responseMessage[MessageKeys.Content]);
+            }
+            catch (Exception ex)
+            {
+                Logger.Instance.Error(ex, "Error while trying to get mosh path.");
+
+                return null;
+            }
+
+            if (!response.Success)
+            {
+                Logger.Instance.Error($"Error while trying to get username. Message: {response.Error}");
+
+                return null;
+            }
+
+            var path = string.IsNullOrEmpty(response.Path) ? string.Empty : response.Path;
+
+            if (isMosh)
+            {
+                _moshPath = path;
+            }
+            else
+            {
+                _sshPath = path;
+            }
+
+            return path;
+        }
+
         public async Task SaveTextFileAsync(string path, string content)
         {
             IDictionary<string, string> responseMessage =
@@ -194,7 +250,11 @@ namespace FluentTerminal.App.Services.Implementation
             return new Dictionary<string, string>
             {
                 [MessageKeys.Type] = content.GetType().Name,
-                [MessageKeys.Content] = JsonConvert.SerializeObject(content)
+                [MessageKeys.Content] = JsonConvert.SerializeObject(content, new JsonSerializerSettings()
+                {
+                    PreserveReferencesHandling = PreserveReferencesHandling.All,
+                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+                })
             };
         }
     }

FluentTerminal.App.Services.Test/DialogServiceTests.cs
@@ -23,7 +23,7 @@ namespace FluentTerminal.App.Services.Test
         public void ShowCreateKeyBindingDialog_Default_UsesCreateKeyBindingDialog()
         {
             var createKeyBindingDialog = new Mock<ICreateKeyBindingDialog>();
-            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, () => createKeyBindingDialog.Object, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, () => createKeyBindingDialog.Object, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             dialogService.ShowCreateKeyBindingDialog();
 
@@ -36,7 +36,7 @@ namespace FluentTerminal.App.Services.Test
             var title = string.Empty;
             var content = _fixture.Create<string>();
             var buttons = _fixture.CreateMany<DialogButton>(2);
-            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             Func<Task<DialogButton>> showMessageDialogAsnyc = () => dialogService.ShowMessageDialogAsnyc(title, content, buttons.ElementAt(0), buttons.ElementAt(1));
 
@@ -49,7 +49,7 @@ namespace FluentTerminal.App.Services.Test
             var title = _fixture.Create<string>();
             var content = string.Empty;
             var buttons = _fixture.CreateMany<DialogButton>(2);
-            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             Func<Task<DialogButton>> showMessageDialogAsnyc = () => dialogService.ShowMessageDialogAsnyc(title, content, buttons.ElementAt(0), buttons.ElementAt(1));
 
@@ -61,7 +61,7 @@ namespace FluentTerminal.App.Services.Test
         {
             var title = _fixture.Create<string>();
             var content = _fixture.Create<string>();
-            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             Func<Task<DialogButton>> showMessageDialogAsnyc = () => dialogService.ShowMessageDialogAsnyc(title, content);
 
@@ -75,7 +75,7 @@ namespace FluentTerminal.App.Services.Test
             var content = _fixture.Create<string>();
             var buttons = _fixture.CreateMany<DialogButton>(2);
             var messageDialog = new Mock<IMessageDialog>();
-            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, () => messageDialog.Object, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, () => messageDialog.Object, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             dialogService.ShowMessageDialogAsnyc(title, content, buttons.ElementAt(0), buttons.ElementAt(1));
 
@@ -86,7 +86,7 @@ namespace FluentTerminal.App.Services.Test
         public void ShowProfileSelectionDialogAsync_Default_UsesShellProfileSelectionDialog()
         {
             var shellProfileSelectionDialog = new Mock<IShellProfileSelectionDialog>();
-            var dialogService = new DialogService(() => shellProfileSelectionDialog.Object, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(() => shellProfileSelectionDialog.Object, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, Mock.Of<IInputDialog>, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             dialogService.ShowProfileSelectionDialogAsync();
 
@@ -98,7 +98,7 @@ namespace FluentTerminal.App.Services.Test
         {
             var title = "title";
             var inputDialog = new Mock<IInputDialog>();            
-            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, () => inputDialog.Object, Mock.Of<ISshConnectionInfoDialog>);
+            var dialogService = new DialogService(Mock.Of<IShellProfileSelectionDialog>, Mock.Of<IMessageDialog>, Mock.Of<ICreateKeyBindingDialog>, () => inputDialog.Object, Mock.Of<ISshConnectionInfoDialog>, Mock.Of<ISshShellProfileSelectionDialog>);
 
             dialogService.ShowInputDialogAsync(title);
 

FluentTerminal.App.Services/ApplicationDataContainers.cs
@@ -7,5 +7,6 @@
         public IApplicationDataContainer Themes { get; set; }
         public IApplicationDataContainer KeyBindings { get; set; }
         public IApplicationDataContainer ShellProfiles { get; set; }
+        public IApplicationDataContainer SshShellProfiles { get; set; }
     }
 }

FluentTerminal.App.Services/Constants.cs
@@ -5,5 +5,6 @@
         public const string ThemesContainerName = "Themes";
         public const string KeyBindingsContainerName = "KeyBindings";
         public const string ShellProfilesContainerName = "ShellProfiles";
+        public const string SshShellProfilesContainerName = "SshShellProfiles";
     }
 }

FluentTerminal.App.Services/Dialogs/ISshConnectionInfoDialog.cs
@@ -1,9 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using FluentTerminal.Models;
+using System.Threading.Tasks;
 
 namespace FluentTerminal.App.Services.Dialogs
 {
     public interface ISshConnectionInfoDialog
     {
-        Task<ISshConnectionInfo> GetSshConnectionInfoAsync(ISshConnectionInfo input = null);
+        Task<ISshConnectionInfo> GetSshConnectionInfoAsync(ISshConnectionInfo input);
     }
 }

FluentTerminal.App.Services/Dialogs/ISshShellProfileSelectionDialog.cs
@@ -1,0 +1,12 @@
+﻿using FluentTerminal.Models;
+using System.Threading.Tasks;
+
+namespace FluentTerminal.App.Services.Dialogs
+{
+    public interface ISshShellProfileSelectionDialog
+    {
+        SshShellProfile SelectedProfile { get; }
+
+        Task<SshShellProfile> SelectProfile();
+    }
+}

FluentTerminal.App.Services/EventArgs/NewWindowRequestedEventArgs.cs
@@ -2,6 +2,6 @@
 {
     public class NewWindowRequestedEventArgs : System.EventArgs
     {
-        public bool ShowProfileSelection { get; set; }
+        public Models.Enums.ProfileSelection ShowSelection { get; set; }
     }
 }

commit 689adc129d2245228e5d706b85c9730bd7c6f3e9

Fix crash when any keybinding is changed
This is a new regression introduced in 0065827bf (SSH profile support).
It's not clear to me why these JSON serializer settings were introduced
so there might be a better way to fix this.

FluentTerminal.App.Services/Adapters/ApplicationDataContainerAdapter.cs
@@ -51,12 +51,7 @@ namespace FluentTerminal.App.Services.Adapters
 
         public void WriteValueAsJson<T>(string name, T value)
         {
-            //_applicationDataContainer.Values[name] = JsonConvert.SerializeObject(value);
-            _applicationDataContainer.Values[name] = JsonConvert.SerializeObject(value, new JsonSerializerSettings()
-            {
-                PreserveReferencesHandling = PreserveReferencesHandling.All,
-                ReferenceLoopHandling = ReferenceLoopHandling.Serialize
-            });
+            _applicationDataContainer.Values[name] = JsonConvert.SerializeObject(value);
         }
     }
 }

FluentTerminal.App.Services/Implementation/TrayProcessCommunicationService.cs
@@ -253,11 +253,7 @@ namespace FluentTerminal.App.Services.Implementation
             return new ValueSet
             {
                 [MessageKeys.Type] = content.Identifier,
-                [MessageKeys.Content] = JsonConvert.SerializeObject(content, new JsonSerializerSettings()
-                {
-                    PreserveReferencesHandling = PreserveReferencesHandling.All,
-                    ReferenceLoopHandling = ReferenceLoopHandling.Serialize
-                })
+                [MessageKeys.Content] = JsonConvert.SerializeObject(content)
             };
         }
     }