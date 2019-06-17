f02711ab8adf76ee78697e013de60db205a860c6
Aleksandar Pesic <peske.nis@gmail.com>
2019-06-17 21:21:25 +0200

    Remote SSH selection dialog improved.

--- FluentTerminal.App/Dialogs/SshProfileSelectionDialog.xaml.cs
+++ FluentTerminal.App/Dialogs/SshProfileSelectionDialog.xaml.cs
@@ -4,8 +4,10 @@ using FluentTerminal.App.Utilities;
 using FluentTerminal.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace FluentTerminal.App.Dialogs
@@ -13,15 +15,25 @@ namespace FluentTerminal.App.Dialogs
     public sealed partial class SshProfileSelectionDialog : ContentDialog, ISshProfileSelectionDialog
     {
         private IDialogService _dialogService;
-        public IEnumerable<SshProfile> Profiles { get; }
+
+        public ObservableCollection<SshProfile> Profiles { get; }
 
         public SshProfile SelectedProfile { get; private set; }
 
         public SshProfileSelectionDialog(ISettingsService settingsService, IDialogService dialogService)
         {
             _dialogService = dialogService;
-            Profiles = settingsService.GetSshProfiles();
-            SelectedProfile = Profiles.FirstOrDefault();
+            Profiles = new ObservableCollection<SshProfile>(settingsService.GetSshProfiles().OrderBy(p => p.Name));
+
+            var selectedProfile = settingsService.GetDefaultSshProfile();
+
+            if (selectedProfile != null)
+            {
+                selectedProfile = Profiles.FirstOrDefault(p => p.Id.Equals(selectedProfile.Id));
+            }
+
+            SelectedProfile = selectedProfile ?? Profiles.FirstOrDefault();
+
             this.InitializeComponent();
             var currentTheme = settingsService.GetCurrentTheme();
             RequestedTheme = ContrastHelper.GetIdealThemeForBackgroundColor(currentTheme.Colors.Background);
@@ -29,21 +41,14 @@ namespace FluentTerminal.App.Dialogs
 
         public async Task<SshProfile> SelectProfile()
         {
-            if (Profiles.Count() == 0)
+            if (!Profiles.Any())
             {
-                _dialogService.ShowMessageDialogAsnyc("SSH Profile Selection", "There are no saved SSH Profiles.", DialogButton.OK);
+                await _dialogService.ShowMessageDialogAsnyc("SSH Profile Selection", "There are no saved SSH Profiles.",
+                    DialogButton.OK);
                 return null;
             }
-            else
-            {
-                var result = await ShowAsync();
-                if (result == ContentDialogResult.Primary)
-                {
-                    return SelectedProfile;
-                }
 
-                return null;
-            }
+            return await ShowAsync() == ContentDialogResult.Primary ? SelectedProfile : null;
         }
     }
 }
