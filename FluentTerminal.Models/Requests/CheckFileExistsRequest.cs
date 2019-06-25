9ca220facf1b400482390316871e0aa68ebb2c94
Aleksandar Pesic <peske.nis@gmail.com>
2019-06-25 04:41:58 +0200

    Preparing infrastructure for file checking.

---
+++ FluentTerminal.Models/Requests/CheckFileExistsRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentTerminal.Models.Requests
+{
+    public class CheckFileExistsRequest : IMessage
+    {
+        public const byte Identifier = 12;
+
+        byte IMessage.Identifier => Identifier;
+
+        public string Path { get; set; }
+    }
+}
