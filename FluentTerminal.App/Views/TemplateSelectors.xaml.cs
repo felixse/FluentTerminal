9a6b760f2a2c4270ae7871f85d1dc5d859bb6c69
Aleksandar Pesic <peske.nis@gmail.com>
2019-06-27 14:18:05 +0200

    Fixing an issue with TemplateSelectors.

---
+++ FluentTerminal.App/Views/TemplateSelectors.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentTerminal.App.Views
+{
+    // Added due https://docs.microsoft.com/en-us/windows/uwp/data-binding/data-binding-in-depth#resource-dictionaries-with-xbind
+    public partial class TemplateSelectors
+    {
+        public TemplateSelectors()
+        {
+            InitializeComponent();
+        }
+    }
+}
