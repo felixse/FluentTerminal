17c2b9b9b289d9ca400425a0f42e2a91b475b6d5
YohskDiSta <yohsk10@gmail.com>
2019-10-10 22:26:56 +0200

    Create the ImageFile object

---
+++ FluentTerminal.Models/ImageFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FluentTerminal.Models
+{
+    public class ImageFile
+    {
+        public ImageFile(string name,
+                         string fileType,
+                         string path,
+                         string base64String)
+        {
+            Name = name;
+            FileType = fileType;
+            Path = path;
+            Base64String = base64String;
+        }
+
+        public string Name { get; }
+        public string FileType { get; }
+        public string Path { get; }
+        public string Base64String { get; }
+    }
+}
