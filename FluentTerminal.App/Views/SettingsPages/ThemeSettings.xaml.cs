ab5a12965cb58937bd9b6b19d1b22307a6c6d42b
YohskDiSta <yohsk10@gmail.com>
2019-10-11 06:58:42 +0200

    Subscribe to background image event and change background in code behind

--- FluentTerminal.App/Views/SettingsPages/ThemeSettings.xaml.cs
+++ FluentTerminal.App/Views/SettingsPages/ThemeSettings.xaml.cs
@@ -1,7 +1,12 @@
-﻿using FluentTerminal.App.Utilities;
+﻿using System;
+using FluentTerminal.App.Utilities;
+using FluentTerminal.App.ViewModels;
 using FluentTerminal.App.ViewModels.Settings;
+using FluentTerminal.Models;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 namespace FluentTerminal.App.Views.SettingsPages
@@ -22,15 +27,26 @@ namespace FluentTerminal.App.Views.SettingsPages
             {
                 ViewModel = viewModel;
                 viewModel.SelectedThemeBackgroundColorChanged += OnSelectedThemeBackgroundColorChanged;
+                viewModel.SelectedThemeBackgroundImageChanged += OnSelectedThemeBackgroundImageChanged;
+
                 var theme = ContrastHelper.GetIdealThemeForBackgroundColor(ViewModel.SelectedTheme.Background);
+
                 SetTheme(theme);
+                SetGridBackground(ViewModel.SelectedTheme.Background, ViewModel.SelectedTheme.BackgroundThemeFile);
             }
+        }
+
+        private void OnSelectedThemeBackgroundImageChanged(object sender, ImageFile e)
+        {
+            SetGridBackground(ViewModel.SelectedTheme.Background, e);
         }
 
         private void OnSelectedThemeBackgroundColorChanged(object sender, string e)
         {
             var theme = ContrastHelper.GetIdealThemeForBackgroundColor(e);
             SetTheme(theme);
+
+            SetGridBackground(e, ViewModel.SelectedTheme.BackgroundThemeFile);
         }
 
         private void SetTheme(ElementTheme theme)
@@ -44,6 +60,33 @@ namespace FluentTerminal.App.Views.SettingsPages
             CloneButton.RequestedTheme = theme;
 
             ContrastHelper.SetTitleBarButtonsForTheme(theme);
+        }
+
+        private void SetGridBackground(string color, ImageFile imageFile)
+        {
+            Brush backgroundBrush;
+
+            if (imageFile != null)
+            {
+                backgroundBrush = new ImageBrush()
+                {
+                    ImageSource = new BitmapImage(new Uri(
+                        imageFile.Path,
+                        UriKind.Absolute)),
+                };
+            }
+            else
+            {
+                backgroundBrush = new AcrylicBrush
+                {
+                    BackgroundSource = AcrylicBackgroundSource.HostBackdrop,
+                    FallbackColor = color.FromString(),
+                    TintColor = color.FromString(),
+                    TintOpacity = ViewModel.BackgroundOpacity
+                };
+            }
+
+            Root.Background = backgroundBrush;
         }
     }
 }
