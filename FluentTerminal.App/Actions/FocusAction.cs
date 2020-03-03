93eccc34706e1774652a84bddbd2094d4dd367ed
Felix Seidl <f.seidl@me.com>
2020-03-03 21:33:05 +0100

    Localize search options, focus search on open

---
+++ FluentTerminal.App/Actions/FocusAction.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FluentTerminal.App.Actions
+{
+    public class FocusAction : DependencyObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            var control = TargetObject ?? sender as Control;
+            control?.Focus(FocusState.Programmatic);
+
+            return null;
+        }
+
+        public Control TargetObject
+        {
+            get { return (Control)GetValue(TargetObjectProperty); }
+            set { SetValue(TargetObjectProperty, value); }
+        }
+
+        public static readonly DependencyProperty TargetObjectProperty =
+            DependencyProperty.Register(nameof(TargetObject), typeof(Control), typeof(FocusAction), new PropertyMetadata(null));
+    }
+}
