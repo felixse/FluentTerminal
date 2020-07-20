ae1f35b58111b5ecdad1d7361b526d15d5290351
RhavoX <rhavox@gmail.com>
2020-07-20 20:07:39 +0200

    Fixed almost all warnings in the project. Only lanuage warning remains for the bundle project due to weird AZ language detection issues.

--- FluentTerminal.App/Properties/AssemblyInfo.cs
+++ FluentTerminal.App/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -26,3 +27,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
--- FluentTerminal.Models/TabTheme.cs
+++ FluentTerminal.Models/TabTheme.cs
@@ -1,7 +1,14 @@
-﻿namespace FluentTerminal.Models
+﻿using System.ComponentModel;
+
+namespace FluentTerminal.Models
 {
-    public class TabTheme
+    public class TabTheme : INotifyPropertyChanged
     {
+        // warning disabled as the PropertyChanged is actually unnecessary here but caused a binding warning
+#pragma warning disable CS0067 // The event 'TabTheme.PropertyChanged' is never used FluentTerminal.Models
+        public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS0067 // The event 'TabTheme.PropertyChanged' is never used FluentTerminal.Models
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Color { get; set; }
