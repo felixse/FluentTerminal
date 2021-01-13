70489186662cc9302036563067b6f51d3b272fc0
akanealw <akanealw@gmail.com>
2021-01-13 14:27:15 -0600

    Update Install_with_icon.bat

--- Explorer Context Menu Integration/Install_with_icon.bat
+++ Explorer Context Menu Integration/Install_with_icon.bat
@@ -1,6 +1,8 @@
-reg add "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%1\"" /f
+reg add "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%V\"" /f
 reg add "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here" /v icon /t REG_SZ /d "%USERPROFILE%\\Pictures\\Icons\\FluentTerminal.ico" /f
 reg add "HKCU\Software\Classes\Directory\Background\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%V\"" /f
 reg add "HKCU\Software\Classes\Directory\Background\shell\Open Fluent Terminal here" /v icon /t REG_SZ /d "%USERPROFILE%\\Pictures\\Icons\\FluentTerminal.ico" /f
+reg add "HKCU\Software\Classes\Drive\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%V\"" /f
+reg add "HKCU\Software\Classes\Drive\shell\Open Fluent Terminal here" /v icon /t REG_SZ /d "%USERPROFILE%\\Pictures\\Icons\\FluentTerminal.ico" /f
 reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%V\"" /f
 reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Fluent Terminal here" /v icon /t REG_SZ /d "%USERPROFILE%\\Pictures\\Icons\\FluentTerminal.ico" /f
