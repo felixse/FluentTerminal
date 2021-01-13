b5f7184141f8fe4d229c5d0f8e084c1483c0dd15
akanealw <akanealw@gmail.com>
2021-01-13 14:28:37 -0600

    Update Uninstall.bat
    Added extra uninstall command for added drive context menu item

--- Explorer Context Menu Integration/Uninstall.bat
+++ Explorer Context Menu Integration/Uninstall.bat
@@ -1,3 +1,4 @@
 reg delete "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here" /f
 reg delete "HKCU\Software\Classes\Directory\Background\shell\Open Fluent Terminal here" /f
+reg delete "HKCU\Software\Classes\Drive\shell\Open Fluent Terminal here" /f
 reg delete "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Fluent Terminal here" /f
