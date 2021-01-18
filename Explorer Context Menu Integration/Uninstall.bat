6a123ed953242860c30c7f1279df29ad374a46e7
akanealw <akanealw@gmail.com>
2021-01-18 15:13:24 -0600

    Made changes to install.bat and added install_with_icon.bat to fix issues #867 and #719 respectively (#884)
    * Create README.md
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Update Install.bat
    
    Added entry for drive context menu item.
    
    * Update Install_with_icon.bat
    
    * Update Uninstall.bat
    
    Added extra uninstall command for added drive context menu item
    
    * Update Install_with_icon.bat
    
    Added another entry for drive context menu integration
    
    * Update Install.bat
    
    * Update README.md
    
    * Update Install_with_icon.bat
    
    * Update README.md
    
    * Update Install_with_icon.bat
    
    * remove icon file on uninstall
    
    Co-authored-by: Felix Seidl <f.seidl@me.com>

--- Explorer Context Menu Integration/Uninstall.bat
+++ Explorer Context Menu Integration/Uninstall.bat
@@ -1,3 +1,5 @@
 reg delete "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here" /f
 reg delete "HKCU\Software\Classes\Directory\Background\shell\Open Fluent Terminal here" /f
+reg delete "HKCU\Software\Classes\Drive\shell\Open Fluent Terminal here" /f
 reg delete "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Fluent Terminal here" /f
+del %LOCALAPPDATA%\Microsoft\WindowsApps\FluentTerminal.ico
