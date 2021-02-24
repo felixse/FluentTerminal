a6f297e366bf3f1909f76eabe4dde1aa6699cb32
J04n5mith <diaetassis.da2@gmx.de>
2021-02-24 15:28:25 +0100

    added quotes in the icon copy line (first line)
    The icon does not copy when you have a path with blanks.

--- Explorer Context Menu Integration/Install_with_icon.bat
+++ Explorer Context Menu Integration/Install_with_icon.bat
@@ -1,4 +1,4 @@
-copy /y FluentTerminal.ico %LOCALAPPDATA%\Microsoft\WindowsApps
+copy /y FluentTerminal.ico "%LOCALAPPDATA%\Microsoft\WindowsApps"
 reg add "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%V\"" /f
 reg add "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here" /v icon /t REG_SZ /d "%LOCALAPPDATA%\Microsoft\WindowsApps\FluentTerminal.ico" /f
 reg add "HKCU\Software\Classes\Directory\Background\shell\Open Fluent Terminal here\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\flute.exe\" new \"%%V\"" /f
