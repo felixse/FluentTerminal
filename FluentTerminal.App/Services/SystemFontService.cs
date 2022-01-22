a76551abb996dba4e35355c939eb2d061fdd8e46
beyond-danube <50598535+beyond-danube@users.noreply.github.com>
2022-01-22 14:06:54 +0200

    Fixing #957 (#993)
    * #957 - using CanvasTextFormat instead of CanvasFontSet. this drops Monospace Fonts highlighting, while resolves a crash at least.
    
    * Revert "highlight monospaced fonts"
    
    This reverts commit 2d204e48abc21838b9af35c33f32d2c19b6ca208.
    
    # Conflicts resolved:
    #	FluentTerminal.App.ViewModels/Settings/TerminalPageViewModel.cs
    #	FluentTerminal.App/App.xaml
    #	FluentTerminal.App/Services/SystemFontService.cs
    #	FluentTerminal.Models/FontInfo.cs
    
    * Fixes #957 - Revert "highlight monospaced fonts"
    
    This reverts commit 2d204e48abc21838b9af35c33f32d2c19b6ca208.
    
    # Conflicts resolved:
    #	FluentTerminal.App.ViewModels/Settings/TerminalPageViewModel.cs
    #	FluentTerminal.App/App.xaml
    #	FluentTerminal.App/Services/SystemFontService.cs
    #	FluentTerminal.Models/FontInfo.cs

--- FluentTerminal.App/Services/SystemFontService.cs
+++ FluentTerminal.App/Services/SystemFontService.cs
@@ -1,28 +1,14 @@
-﻿using FluentTerminal.Models;
-using Microsoft.Graphics.Canvas.Text;
+﻿using Microsoft.Graphics.Canvas.Text;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace FluentTerminal.App.Services
 {
     public class SystemFontService : ISystemFontService
     {
-        public IEnumerable<FontInfo> GetSystemFontFamilies()
+        public IEnumerable<string> GetSystemFontFamilies()
         {
-            return CanvasFontSet.GetSystemFontSet().Fonts.Select(x => new FontInfo { Name = x.FamilyNames.Values.FirstOrDefault(), IsMonospaced = x.IsMonospaced }).Distinct(new FontInfoComparer());
+            return CanvasTextFormat.GetSystemFontFamilies();
         }
 
-        private class FontInfoComparer : IEqualityComparer<FontInfo>
-        {
-            public bool Equals(FontInfo x, FontInfo y)
-            {
-                return x.Name == y.Name;
-            }
-
-            public int GetHashCode(FontInfo obj)
-            {
-                return obj.Name.GetHashCode();
-            }
-        }
     }
 }
